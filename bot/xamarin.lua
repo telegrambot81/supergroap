8b1460185585e490c40d6b77b7aa8074ab7dae94
telegrambot81 <telegrambot81@gmail.com>
2016-04-02 21:29:01 +0430

    Update xamarin.lua

--- bot/xamarin.lua
+++ bot/xamarin.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {119626024,0,tonumber(our_id)},
+    sudo_users = {120629055,106789762},
     admin_users = {},
     disabled_channels = {}
   }
